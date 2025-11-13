a7186dfe1ca7425a1829712ee7f844becbca1b06
Nawhuna <nguyennam2017an@gmail.com>
2025-11-14 00:11:08 +0700

    thêm chức năng chat khi chơi

--- GAMECARO/Forms/FormMain.Designer.cs
+++ GAMECARO/Forms/FormMain.Designer.cs
@@ -1,11 +1,20 @@
-﻿namespace Client.Forms
+﻿using System.Windows.Forms;
+using System.Drawing;
+
+namespace Client.Forms
 {
     partial class FormMain
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer? components = null;
+
+        // 🔹 Các control UI
+        private TextBox txtPlayerName;
+        private Button btnConnect;
+        private Label lblYou;
+        private Label lblEnemy;
+        private TextBox txtChat;
+        private TextBox txtMessage;
+        private Button btnSend;
 
         /// <summary>
         ///  Clean up any resources being used.
@@ -23,7 +32,98 @@
 
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            this.txtPlayerName = new TextBox();
+            this.btnConnect = new Button();
+            this.lblYou = new Label();
+            this.lblEnemy = new Label();
+            this.txtChat = new TextBox();
+            this.txtMessage = new TextBox();
+            this.btnSend = new Button();
+            this.SuspendLayout();
+            // 
+            // txtPlayerName
+            // 
+            this.txtPlayerName.Location = new Point(400, 50);   // dưới thanh top, bên phải bàn cờ
+            this.txtPlayerName.Name = "txtPlayerName";
+            this.txtPlayerName.Size = new Size(150, 23);
+            this.txtPlayerName.TabIndex = 0;
+            this.txtPlayerName.Text = "Nam";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new Point(560, 49);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new Size(75, 23);
+            this.btnConnect.TabIndex = 1;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // lblYou
+            // 
+            this.lblYou.AutoSize = true;
+            this.lblYou.Location = new Point(400, 80);
+            this.lblYou.Name = "lblYou";
+            this.lblYou.Size = new Size(60, 15);
+            this.lblYou.TabIndex = 2;
+            this.lblYou.Text = "You: N/A";
+            // 
+            // lblEnemy
+            // 
+            this.lblEnemy.AutoSize = true;
+            this.lblEnemy.Location = new Point(400, 100);
+            this.lblEnemy.Name = "lblEnemy";
+            this.lblEnemy.Size = new Size(76, 15);
+            this.lblEnemy.TabIndex = 3;
+            this.lblEnemy.Text = "Enemy: N/A";
+            // 
+            // txtChat
+            // 
+            this.txtChat.Location = new Point(400, 130);
+            this.txtChat.Multiline = true;
+            this.txtChat.Name = "txtChat";
+            this.txtChat.ReadOnly = true;
+            this.txtChat.ScrollBars = ScrollBars.Vertical;
+            this.txtChat.Size = new Size(370, 380);
+            this.txtChat.TabIndex = 4;
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new Point(400, 520);
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new Size(260, 23);
+            this.txtMessage.TabIndex = 5;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new Point(670, 519);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new Size(100, 23);
+            this.btnSend.TabIndex = 6;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new SizeF(7F, 15F);
+            this.AutoScaleMode = AutoScaleMode.Font;
+            this.ClientSize = new Size(800, 600);   // rộng hơn cho chat
+            this.Name = "FormMain";
+            this.Text = "Caro Online 15x15";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+
+            // 🔹 Thêm control vào form
+            this.Controls.Add(this.txtPlayerName);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.lblYou);
+            this.Controls.Add(this.lblEnemy);
+            this.Controls.Add(this.txtChat);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.btnSend);
+
+            this.ResumeLayout(false);
+            this.PerformLayout();
         }
 
         #endregion
--- GAMECARO/Forms/FormMain.cs
+++ GAMECARO/Forms/FormMain.cs
@@ -1,32 +1,34 @@
-﻿using Client.Game;
-using Client.Network;
+﻿using System;
 using System.Drawing;
 using System.Windows.Forms;
+using Client.Game;
+using Client.Network;
 
 namespace Client.Forms
 {
     public partial class FormMain : Form
     {
-        // // thanh phan logic !! khong duoc dung vao " Trinh"
+        // thanh phan logic
         private readonly Board _board = new();
         private GameRenderer _renderer;
         private GameLoop _loop;
         private ConnectToServer _net;
+
         // thanh phan giao dien
         private Panel _canvas;
         private Label _lblTurn;
         private Label _lblMark;
         private Label _lblTimer;
 
-        // giao dien chinh 
         public FormMain()
         {
             InitializeComponent();
+
             this.Text = "Caro Online 15x15";
-            this.ClientSize = new Size(580, 620);
+            this.ClientSize = new Size(800, 620); // rộng hơn tí cho khu chat
             this.DoubleBuffered = true;
 
- 
+            // ====== PANEL TRÊN: TURN / YOU / TIMER ======
             var top = new Panel
             {
                 Dock = DockStyle.Top,
@@ -34,6 +36,7 @@ namespace Client.Forms
                 Padding = new Padding(8),
                 BackColor = Color.LightGray
             };
+
             _lblTurn = new Label
             {
                 AutoSize = true,
@@ -54,25 +57,27 @@ namespace Client.Forms
                 Text = "⏱️ 30s",
                 Font = new Font("Arial", 12, FontStyle.Bold)
             };
+
             top.Controls.Add(_lblTurn);
             top.Controls.Add(_lblMark);
             top.Controls.Add(_lblTimer);
             this.Controls.Add(top);
 
-            
+            // ====== PANEL VẼ BÀN CỜ ======
             _canvas = new Panel
             {
-                Dock = DockStyle.Fill,
+                Dock = DockStyle.Left,
+                Width = 380,
                 BackColor = Color.White
             };
             this.Controls.Add(_canvas);
 
-            // render + loop game
+            // ====== RENDER + GAME LOOP ======
             _renderer = new GameRenderer(_canvas, _board);
             _loop = new GameLoop(this);
             _loop.Start();
 
-            // ham connect to server khong duoc sua
+            // ====== KẾT NỐI SERVER (KHÔNG SỬA HÀM CONNECT INSIDE ConnectToServer) ======
             _net = new ConnectToServer("127.0.0.1", 9000, _board);
 
             _net.OnBoardChanged += () => this.BeginInvoke(new Action(() =>
@@ -97,8 +102,7 @@ namespace Client.Forms
             _net.OnTimerUpdate += (sec, turn) => this.BeginInvoke(new Action(() =>
             {
                 _lblTimer.Text = $"⏱️ {turn}: {sec}s";
-                if (sec <= 5) _lblTimer.ForeColor = Color.Red;
-                else _lblTimer.ForeColor = Color.Black;
+                _lblTimer.ForeColor = sec <= 5 ? Color.Red : Color.Black;
             }));
 
             _net.OnReset += () => this.BeginInvoke(new Action(() =>
@@ -108,9 +112,10 @@ namespace Client.Forms
                 _renderer.Refresh();
             }));
 
+            // hiện tại vẫn dùng tên mặc định của Windows
             _net.ConnectAndJoin(Environment.UserName);
 
-            // xuw lys click chuot ko duoc dung vao " Trinh"
+            // xử lý click chuột trên bàn cờ
             _canvas.MouseClick += (s, e) =>
             {
                 var cell = _renderer.PointToCell(e.Location);
@@ -126,12 +131,58 @@ namespace Client.Forms
                 _net.SendMove(cell.Value.row, cell.Value.col);
             };
         }
-        // xu ly khi close game
+
+        // ====== HÀM XỬ LÝ NÚT CONNECT (UI) ======
+        private void btnConnect_Click(object? sender, EventArgs e)
+        {
+            // tạm thời chỉ lấy tên và in ra khung chat
+            string name = txtPlayerName.Text.Trim();
+            if (string.IsNullOrEmpty(name))
+                name = Environment.UserName;
+
+            AppendChat($"[SYSTEM] Bạn đang dùng tên: {name}");
+
+            // TODO: nếu muốn dùng tên này để join server thì sau này
+            // có thể sửa _net.ConnectAndJoin(name) và bỏ ConnectAndJoin ở constructor.
+        }
+
+        // ====== HÀM XỬ LÝ NÚT SEND CHAT ======
+        private void btnSend_Click(object? sender, EventArgs e)
+        {
+            string msg = txtMessage.Text.Trim();
+            if (string.IsNullOrEmpty(msg)) return;
+
+            // hiện tại mới chat local cho dễ test
+            AppendChat($"You: {msg}");
+            txtMessage.Clear();
+
+            // TODO: khi có hàm gửi chat trong ConnectToServer, sẽ gọi _net.SendChat(msg) ở đây.
+        }
+
+        // ====== THÊM DÒNG VÀO KHUNG CHAT ======
+        private void AppendChat(string line)
+        {
+            if (txtChat.InvokeRequired)
+            {
+                txtChat.Invoke(new Action(() => AppendChat(line)));
+            }
+            else
+            {
+                txtChat.AppendText(line + Environment.NewLine);
+            }
+        }
+
+        // xử lý khi close game
         protected override void OnFormClosed(FormClosedEventArgs e)
         {
             _loop?.Stop();
             _net?.Disconnect();
             base.OnFormClosed(e);
+        }
+
+        private void FormMain_Load(object? sender, EventArgs e)
+        {
+            // hiện tại chưa cần làm gì khi load
         }
     }
 }
